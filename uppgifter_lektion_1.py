878909e3d2131fc288b160ba6db8eefd81fd2222
SweJob <31406955+SweJob@users.noreply.github.com>
2024-09-18 18:08:51 +0200

    Adding uppgifter

---
+++ uppgifter_lektion_1.py
@@ -0,0 +1,44 @@
+#uppgif5
+
+name = "Jonas"
+age = 50
+temperature = 17.2
+married = True
+
+print(name, type(name))
+print(age, type(age))
+print(temperature, type(temperature))
+print(married, type(married))
+
+#uppgift6
+
+a = 15
+b = 4
+summa = a + b
+subtr = a - b
+product =  a * b
+quota = a / b
+int_divisor = a // b
+modulus = a % b
+exponent = a**b
+
+print ("A=",a,"\tB=",b)
+print ("A + B  =\t", summa)
+print ("A - B  =\t", subtr)
+print ("A x B  =\t", product)
+print ("A x B  =\t", product)
+print ("A x B  =\t", product)
+print ("A / B  =\t", quota)
+print ("A // B =\t", int_divisor)
+print ("A % B  =\t", modulus)
+print ("A ^ B  =\t", exponent)
+
+#uppgift7
+
+text = "Hello, World!"
+print (text)
+print (text[0])
+print (text[6:12])
+print (text[0:len(text):2])
+print (text[-1::-1])
+
